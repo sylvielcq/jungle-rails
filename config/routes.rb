c8ff097879443ea0e20cb157cbc786a73eafcaa2
sylvielcq <slcq.dev@gmail.com>
2022-09-22 17:07:40 -0700

    Add routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   root to: 'products#index'
   get 'about/', to: "about#index"
 
+  get '/signup', to: 'users#new'
+  post '/users', to: 'users#create'
+
   resources :products, only: [:index, :show]
   resources :categories, only: [:show]
 
