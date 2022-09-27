d0a5912f43472dc50c9f90ad6d6aef7ce3b203e0
sylvielcq <slcq.dev@gmail.com>
2022-09-26 22:55:16 -0700

    write cypress tests for home page

---
+++ cypress/integration/home.spec.js
@@ -0,0 +1,13 @@
+describe('Home Page', () => {
+  it("Navigates to homepage", () => {
+    cy.visit("/");
+  });
+
+  it("There are products on the page", () => {
+    cy.get(".products article").should("be.visible");
+  });
+
+  it("There are 2 products on the page", () => {
+    cy.get(".products article").should("have.length", 2);
+  });
+});
